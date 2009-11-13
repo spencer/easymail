8da2c8eedb70778a4ed79774b04ee8c65b8925bd
Spencer Turner <spencer@edendevelopment.co.uk>
2009-11-13 13:57:38 +0000

    Added lost of nasty untested code...hackday breeds bad habits!

--- easymail.rb
+++ easymail.rb
@@ -1,13 +1,39 @@
+#!/usr/bin/ruby
+
 require 'rubygems'
 require 'sinatra'
+
 require 'haml'
 require 'sass'
-require 'lib/partials'
-include Sinatra::Partials
-
 set :haml, {:format => :html5 } # default Haml format is :xhtml
 set :sass, {:format => :html5 } # default Haml format is :
 
+require 'lib/partials'
+include Sinatra::Partials
+
+require 'hpricot'
+require 'tamtam'
+
+require 'dm-core'
+require 'dm-timestamps'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/mymail.db")
+
+class MyMail
+    include DataMapper::Resource
+    property :id, Serial
+    property :to, String
+    property :salutation, String
+    property :from, String
+    property :title, String
+    property :message, Text
+    property :body, Text
+    property :created_at, DateTime
+    property :updated_at, DateTime
+end
+
+# automatically create the post table
+MyMail.auto_upgrade!
 
 helpers do
 end
@@ -21,13 +47,44 @@ get '/styles/:style.css' do
 end
  
 get '/' do
-  haml :index
+  haml :new
 end
 
-not_found do
-  haml :'404'
+post '/' do
+  @mymail = MyMail.new(params[:mymail])
+  @mymail.body = process_body
+  
+  if @mymail.save
+    redirect("/#{@mymail.id}")
+  else
+    redirect('/')
+  end
 end
 
-error do
-  haml :'500'
+get '/list' do
+  @mymails = MyMail.all
+  if @mymails.size > 0
+  haml :list
+  else
+    redirect('/')
+  end
+end
+
+get '/:id' do
+  @mymail = MyMail.get(params[:id])
+  haml :show
+end
+
+get '/:id/delete' do
+  @mymail = MyMail.get(params[:id])
+  @mymail.destroy!
+  redirect '/list'
+end
+
+def process_body
+  @body = Hpricot.parse(params[:template][:tempfile].read)
+  (@body/'#message').inner_html = @mymail.message
+  (@body/'#salutation').inner_html = @mymail.salutation
+  doc = @body.to_s
+  @inlined_body = TamTam.inline(:document => doc)
 end
--- specs/easymailer_spec.rb
+++ specs/easymailer_spec.rb
@@ -3,14 +3,14 @@ require File.dirname(__FILE__) + '/spec_helper'
 describe "EasyMailer" do
   include Rack::Test::Methods
 
-  # def app
-  #   @app ||= Sinatra::Application
-  # end
-  # 
-  # it "should respond to /" do
-  #   get '/'
-  #   last_response.should be_ok
-  # end
+   def app
+     @app ||= Sinatra::Application
+   end
+   
+   it "should respond to /" do
+     get '/'
+     last_response.should be_ok
+   end
   # 
   # it "should return the correct content-type when viewing root" do
   #   get '/'
